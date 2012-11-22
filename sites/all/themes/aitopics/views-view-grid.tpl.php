7d3257a28b0354b2e7b2b03ae797f3a3988d9443
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2012-11-22 12:53:03 -0500

    Added file forgotten in last commit.

---
+++ sites/all/themes/aitopics/views-view-grid.tpl.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file views-view-grid.tpl.php
+ * Default simple view template to display a rows in a grid.
+ *
+ * - $rows contains a nested array of rows. Each row contains an array of
+ *   columns.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)) : ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+
+<?php foreach ($rows as $columns): ?>
+  <div style="clear: both">
+    <?php foreach ($columns as $counter => $item): ?>
+      <div class="grid-item-<?php if($counter % 2 == 0) { echo "left"; } else { echo "right"; } ?>">
+        <?php print $item; ?>
+      </div>
+    <?php endforeach; ?>
+  </div>
+<?php endforeach; ?>
+<div style="clear: both;" />
