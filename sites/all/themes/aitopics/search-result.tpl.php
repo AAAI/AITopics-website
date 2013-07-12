73f0d7bee7692275e9af4f86f2098042ef4b2ea4
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2013-07-12 08:28:46 -0700

    Properly highlight authors/source field in search results.

--- sites/all/themes/aitopics/search-result.tpl.php
+++ sites/all/themes/aitopics/search-result.tpl.php
@@ -119,13 +119,13 @@ if(!empty($result['fields']['im_field_topics'])) {
       <?php
         $metadata = array();
         if(!empty($result['snippets']['tos_field_authors'])) {
-          array_push($metadata, $result['snippets']['tos_field_authors']);
+          array_push($metadata, $result['snippets']['tos_field_authors'][0]);
         } elseif(!empty($result['fields']['tos_field_authors'])) {
           array_push($metadata, $result['fields']['tos_field_authors']);
         }
 
         if(!empty($result['snippets']['tos_field_source'])) {
-          array_push($metadata, $result['snippets']['tos_field_source']);
+          array_push($metadata, $result['snippets']['tos_field_source'][0]);
         } elseif(!empty($result['fields']['tos_field_source'])) {
           array_push($metadata, $result['fields']['tos_field_source']);
         }
@@ -144,6 +144,7 @@ if(!empty($result['fields']['im_field_topics'])) {
         print implode("<br/>", $metadata);
         if(!empty($metadata)) { print "<br/>"; }
       ?>
+
       <?php if(user_access('administer nodes')): ?>
       &mdash; <a href="<?php print "/node/".$result['fields']['entity_id']; ?>">View</a> | <a href="<?php print "/node/".$result['fields']['entity_id']."/edit"; ?>">Edit...</a>
       <?php endif; ?>
